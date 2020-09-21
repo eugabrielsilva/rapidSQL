11acdc7b5408ceabf0686a5aebefa76925930d32
Sayuri <64693687+sayuribotbr@users.noreply.github.com>
2020-09-21 10:08:17 -0300

    minor updates

--- rapidSQL.js
+++ rapidSQL.js
@@ -1,14 +1,27 @@
 const mysql = require('mysql');
 
 class Database{
+    /**
+     * Stores the current connection pool.
+     * @type {mysql.Pool}
+     */
     pool = null;
-    conData = null;
 
+    /**
+     * Stores all connection options.
+     * @type {{host: string, user: string, password: string, database: string, port: number, connectionLimit: number, stringifyObjects: boolean, debug: boolean, supportBigNumbers: boolean, multipleStatements: boolean}}
+     */
+    connectionOptions = null;
+
+    /**
+     * 
+     * @param {{host: string, user: string, password: string, database: string, port: number, connectionLimit: number, stringifyObjects: boolean, debug: boolean}} connectionOptions The connection options to be passed to mysql driver.
+     */
     constructor({host='localhost', user, password, database, port=3306, connectionLimit=10, stringifyObjects=false, debug=false}){
         if(!user) throw 'Please provide a user!';
         if(!password) throw 'Please provide a password!';
         if(!database) throw 'Please provide a database!';
-        this.conData = {
+        this.connectionOptions = {
             host: host,
             user: user,
             password: password,
@@ -27,35 +40,57 @@ class Database{
      */
     connect(){
         return new Promise((resolve, reject) => {
-            if(!this.conData) return reject('No connection data were provided!');
-            this.pool = new mysql.createPool(this.conData);
-            this.pool.getConnection((err, connection) => {
-                if(err){
-                    this.pool = null;
-                    return reject(err);
-                };
-                connection.release();
-                resolve();
-            });
-            this.pool.on('error', function(err){
-                this.pool = null;
-                throw err;
-            });
+            if(!this.connectionOptions) return reject('No connection options were provided!');
+            try{
+                this.pool = new mysql.createPool(this.connectionOptions);
+                this.pool.getConnection((err, connection) => {
+                    if(err){
+                        this.pool = null;
+                        return reject(err);
+                    };
+                    connection.release();
+                    resolve();
+                });
+            }catch(error){
+                throw error;
+            };
         });
     };
 
     /**
      * Runs an SQL query asynchronously.
      * @param {string} q A valid SQL query to run.
-     * @param {(string|string[])} vars Optional values to escape.
+     * @param {*} vars Optional values to escape.
      */
     sql(q, vars=''){
         return new Promise((resolve, reject) => {
             if(!this.pool) return reject('I\'m not connected to any database!');
-            this.pool.query(q, vars, function(err, result){
-                if(err) return reject(err);
-                resolve(result);
-            });
+            try{
+                this.pool.query(q, vars, (err, result) => {
+                    if(err) return reject(err);
+                    resolve(result);
+                });
+            }catch(error){
+                throw error;
+            };
+        });
+    };
+
+    /**
+     * Closes the connection after all pending queries are executed.
+     */
+    close(){
+        return new Promise((resolve, reject) => {
+            if(!this.pool) return reject('I\'m not connected to any database!');
+            try{
+                this.pool.end((err) => {
+                    if(err) return reject(err);
+                });
+                this.pool = null;
+                resolve();
+            }catch(error){
+                throw error;
+            };
         });
     };
 };
